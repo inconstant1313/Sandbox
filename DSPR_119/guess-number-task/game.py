b1ea2ee28e12225bd0f46605e5d29f36692ef599
inconstant1313 <inconstant1313@gmail.com>
2022-10-13 16:10:08 +0500

    Added function to the game.py and changed notebook game.ipynb

--- DSPR_119/guess-number-task/game.py
+++ DSPR_119/guess-number-task/game.py
@@ -2,21 +2,25 @@
 
 import numpy as np
 
-number = np.random.randint(1, 101) # загадываем число
+def random_game():
+    number = np.random.randint(1, 101) # загадываем число
 
-# количество попыток
-count = 0
+    # количество попыток
+    count = 0
 
-while True:
-    count+=1
-    predict_number = int(input("Угадай число от 1 до 100: "))
+    while True:
+        count+=1
+        predict_number = int(input("Угадай число от 1 до 100: "))
     
-    if predict_number > number:
-        print("Число должно быть меньше!")
+        if predict_number > number:
+            print("Число должно быть меньше!")
 
-    elif predict_number < number:
-        print("Число должно быть больше!")
+        elif predict_number < number:
+            print("Число должно быть больше!")
     
-    else:
-        print(f"Вы угадали число! Это число = {number}, за {count} попыток")
-        break #конец игры выход из цикла
+        else:
+            print(f"Вы угадали число! Это число = {number}, за {count} попыток")
+            break #конец игры выход из цикла
+    return("You win!!!")    
+
+#print(random_game()) # для запуска игры из этого файла уберите #
